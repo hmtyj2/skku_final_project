0947f3f1d509963f68462dca7168f13491855999
hmtyj2 <ghkdalsxkr@naver.com>
2020-10-19 19:13:30 +0900

    parser,crawler,tf_idf,contents_print

---
+++ crawling.py
@@ -0,0 +1,174 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from khaiii import KhaiiiApi
+from tf_idf import Tf_idf
+import parsing
+import contents_print
+
+api = KhaiiiApi('/home/hwang/khaiii/khaiii/build/lib/libkhaiii.so.0.4', '/home/hwang/khaiii/khaiii/build/share/khaiii')
+
+max_depth = 1
+url = 'https://www.mma.go.kr/index.do'
+filter_domain='mma.go.kr/index.do'
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/home/hwang/chromedriver_linux64/chromedriver',chrome_options=chrome_options)
+driver.implicitly_wait(3)
+driver.set_page_load_timeout(100)
+
+visited_pages = set()
+keywords = []
+df_dict={}  # total word df
+homepages=[]    # total pages visited
+parser_dict = {}
+
+def getPage(tag, urls, flag):  # flag : true(contents mode) false(soup mode)
+    try:
+        driver.get(urls)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+    except:
+        return []
+
+    if flag:
+        if soup.find('html', {'lang': 'ko'}) is None:
+            return []
+
+        contents = soup.find_all(tag)
+        return contents
+    else:
+        return soup
+
+def getLink(urls):
+    sources_tag_a = getPage('a', urls,True)
+    links = []
+    #
+    if urls not in visited_pages:
+        links.append(urls)
+    #
+    for link in sources_tag_a:
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in visited_pages and 'http' in link.attrs['href'] \
+            and filter_domain in link.attrs['href'] and 'pdf' not in link.attrs['href'] \
+            and 'hwp' not in link.attrs['href'] and 'zip' not in link.attrs['href'] and 'login' not in link.attrs['href'] \
+            and 'Login' not in link.attrs['href']:
+                newPage = link.attrs['href']
+                visited_pages.add(newPage)
+                links.append(newPage)
+
+    return links
+
+def update_df_dict(page_df_dict,df_dict):
+    for word in page_df_dict.keys():
+        if word in df_dict.keys():
+            df_dict[word]+=1
+        else:
+            df_dict[word]=1
+    pass
+
+def search(urls, depth):
+    #print(len(keywords),urls)
+    depth += 1
+    if depth > max_depth:
+        return
+
+    links = getLink(urls)
+    keyword = set()
+    #if urls not in visited_pages:
+        #links.insert(0,urls)
+
+    for n in links:
+        print(n)
+        page_norm = []
+        ## add n to tag_parser
+        parser = parsing.Tag_parser(getPage('None',n,False),n)
+
+        ''' test code
+        for i in parser.contents:
+            print('Title : ',i,"  contents : ",parser.contents[i])
+        '''
+        parser_dict[n] = parser
+
+        contents = list(parser.contents.keys())
+
+        for c in list(contents):
+            for text_contents in list(c):
+                for text in text_contents:
+                    if text != '' and 'function' not in text:  # keyword = search word 판단부분
+                        keyword.add(text)
+                        for word in api.analyze(text):
+                            temp = []
+                            for morph in word.morphs:
+                                if 'NN' in morph.tag:
+                                    temp.append(morph.lex)
+                            if len(temp):
+                                page_norm.append(temp)
+        temp_cl=Tf_idf(page_norm,n)
+        temp_df_dict=temp_cl.get_wordFreq_page()    # get word's tf from page
+        homepages.append(temp_cl)
+        update_df_dict(temp_df_dict,df_dict) # update total word's df
+        keywords.append(keyword)
+        pre_parser = parser
+        search(n, depth)
+
+def cal_tf_idf(homepages):
+    for page in homepages:
+        page.get_tf_idf(df_dict,len(keywords))
+    pass
+
+def get_target_page(homepages,input_words):
+    m=10000
+    for page in homepages:
+        score=0
+        for word in input_words:
+            if word in page.word_rank:
+                score+=page.word_rank[word]
+            else:
+                score+=200
+        if score<m:
+            m=score
+            target_page=page.url
+    return target_page
+
+def file_write(homepages,df_dict):
+    with open('result.txt','w') as fp:
+        for page in homepages:
+            fp.write(page.url)
+            fp.write('\n')
+            for line in page.text:
+                fp.write(' '.join(line))
+                fp.write('\n')
+            for x in page.sort_dict():
+                fp.write(''.join('%s %s' %x))
+                fp.write(' %d' %page.word_rank[x[0]])
+                fp.write(' %d' %page.tf_dict[x[0]])
+                fp.write(' %d' %df_dict[x[0]])
+                fp.write('\n')
+
+
+search(url, 0)
+cal_tf_idf(homepages)
+for page in homepages:
+    page.get_word_rank()
+file_write(homepages,df_dict)
+
+
+
+while(1):
+    list1=[]
+    while(1):
+        temp=input("input keywords: ")
+        if temp!='exit':
+            list1.append(temp)
+        else:
+            break
+    print(get_target_page(homepages,list1))
+    target_page_url = get_target_page(homepages, list1)
+
+    for i in parser_dict[target_page_url].contents:
+        print(i," : ",parser_dict[target_page_url].contents[i])
+    print(contents_print.find_contents(parser_dict[target_page_url],list1))
+print("FINISH")
---
+++ parsing.py
@@ -0,0 +1,148 @@
+# -*- coding:utf-8 -*-
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from khaiii import KhaiiiApi
+#import table_reader
+
+class Tag_parser :
+    def __init__(self,soup,url):
+        self.tags = []
+        self.titles = {}
+        self.contents = {}
+        self.stopwords = ['<!','script','function','#']
+        self.api = KhaiiiApi('/home/hwang/khaiii/khaiii/build/lib/libkhaiii.so.0.4', '/home/hwang/khaiii/khaiii/build/share/khaiii')
+        #self.tables=table_reader.get_all_tables(soup)
+        #print(url,self.tables)
+        self.table_count=0
+        '''
+        self.url = 'http://hosp.ajoumc.or.kr/MedicalInfo/HospitalRoomGuide.aspx'
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome('./chrome/chromedriver_linux64/chromedriver', chrome_options=chrome_options)
+        driver.implicitly_wait(3)
+        driver.set_page_load_timeout(100)
+        driver.get(self.url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        '''
+
+        self.recursiveChildren(soup)
+
+    def isstopWord(self,args):
+        for word in self.stopwords:
+            if word in args or '\n' == args or str(type(args)) == "<class 'bs4.element.Comment'>":  # ignore comment
+                return True
+
+        return False
+
+    def imgTagparse(self,args):
+        if 'alt' in args.attrs.keys():
+            return args.attrs['alt']
+        else:
+            return ""
+    def dictvalue_to_list(self, dicts):
+        res_list = []
+        for v in dicts.values():
+            if v is None:
+                v = []
+            elif str(type(v)) == "<class 'bs4.element.NavigableString'>" or str(type(v)) == "<class 'str'>":
+                v = list(v.strip().split())
+            res_list.append(tuple(v))
+
+        return tuple(res_list)
+
+    def extract_words(self,text):
+        temp = []
+        if text == '': return temp
+
+        for word in self.api.analyze(text):
+            for morph in word.morphs:
+                if 'NN' in morph.tag:
+                    temp.append(morph.lex)
+
+        if len(temp):
+            return temp
+
+
+    def parents_name(self,link,tag):
+        name = []
+        if(type(link) == type('')) : return False
+        for p in link.parents:
+            name.append(p.name)
+
+        if(set(tag) & set(name)) : return True
+        else : return False
+
+
+
+
+    def recursiveChildren(self,x):
+        try:
+            for child in x.recursiveChildGenerator():
+                if self.isstopWord(child):
+                    continue
+                name = getattr(child, "name", None)
+
+                if name == 'img':
+                    child = self.imgTagparse(child)
+                    self.tags.append(name)
+                    name = None
+
+                if name is not None:
+
+                    if 'li' == name:
+                        ##### insertion to contents dict code
+                        self.titles['word_from_contents'] = self.extract_words(child.get_text().strip())
+                        self.contents[self.dictvalue_to_list(self.titles)] = [child.get_text().strip()]
+                        ######
+                    # elif 'table' == name:
+                    #     ############  write code here . ########################
+                    #     temp_table=self.tables[self.table_count]
+                    #     self.table_count+=1
+                    #     if not temp_table:
+                    #         continue
+                    #     for line in temp_table:
+                    #         try:
+                    #             self.titles['word_from_contents'] = self.extract_words(line)
+                    #             self.contents[self.dictvalue_to_list(self.titles)] = [line]
+                    #             #print(self.titles,' : ', self.contents[self.dictvalue_to_list(self.titles)])
+                    #         except:
+                    #             pass
+                    #     pass
+                    else:
+                        self.tags.append(name)
+                else:
+                    if child.isspace() or len(self.tags) == 0 or child == '':  # lear node, don't print spaces or non-tag
+                        continue
+                    else:
+                        if self.parents_name(child, ['li','table']):
+                            continue
+
+                        if 'h' in self.tags[-1] or 'img' in self.tags[-1]:  # or 'span' in self.tags[-1]:  # append headline
+                            if 'img' in self.tags[-1] and 'h' in self.tags[-2]: # img tag in headline
+                                self.titles[self.tags[-2]] = child
+                            elif 'h' in self.tags[-1]: # just headline
+                                self.titles[self.tags[-1]] = child
+                        else:
+                            self.titles['word_from_contents'] = self.extract_words(child)
+                            self.contents[self.dictvalue_to_list(self.titles)] = [child.strip()]  # set contents {title : contents}
+                            print(self.titles,' : ', self.contents[self.dictvalue_to_list(self.titles)])
+
+
+                    if len(self.tags):
+                        self.tags.pop(-1)
+
+        except Exception as ex:
+            print("error ",ex)
+            return
+
+
+
+
+# t = Tag_parser()
+# for title in t.contents.keys() :
+#     print('title is ', title ,"contents is ", t.contents[title] )
+    #print(list(title))
